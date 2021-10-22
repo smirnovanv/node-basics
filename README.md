dbc232d944fda51a07c9b19332aa71c409be416f
Natalya Smirnova <natalya.smirnova@akvelon.com>
2021-10-22 13:15:06 +0300

    Add eslint and readme

---
+++ README.md
@@ -0,0 +1,12 @@
+AVAILABLE SCRIPTS
+
+npm run build - to transfer ts into js
+
+node index.js ... - to run the project
+
+example links: 
+node index.js https://filesamples.com/samples/document/txt/sample3.txt https://filesamples.com/samples/document/txt/sample2.txt https://filesamples.com/samples/document/txt/sample1.txt
+
+npm run test - to test the project via jest
+
+npm run lint - to test the project via eslint
--- index.ts
+++ index.ts
@@ -1,30 +1,12 @@
 import * as https from 'https';
 import * as fs from 'fs';
-const URL = require("url").URL;
 import { opendir } from 'fs/promises';
-import { strict as assert } from 'assert';
+
 const Reset = '\x1b[0m';
 const FgRed = '\x1b[31m';
-const FgGreen = '\x1b[32m';
 const FgCyan = '\x1b[36m';
-const Bright = '\x1b[1m';
-const Dim = '\x1b[2m';
-const Underscore = '\x1b[4m';
-const Blink = '\x1b[5m';
-const Reverse = '\x1b[7m';
-const Hidden = '\x1b[8m';
-const FgBlack = '\x1b[30m';
-const FgYellow = '\x1b[33m';
-const FgBlue = '\x1b[34m';
-const FgMagenta = '\x1b[35m';
-const FgWhite = '\x1b[37m';
-const BgBlack = '\x1b[40m';
-const BgRed = '\x1b[41m'
-
 const redText = (str: string) => `${FgRed}${str}${Reset}`;
 const cyanText = (str: string) => `${FgCyan}${str}${Reset}`;
-const greenText = (str: string) => `${FgGreen}${str}${Reset}`;
-
 
 const validUrls: string[] = [];
 
@@ -44,7 +26,7 @@ const downloadFile = (link: string, index: number) => {
       fs.mkdirSync(dir);
   }
   const file = fs.createWriteStream(`./myFiles/file${index}.txt`);
-  const request = https.get(link, function(response) {
+  https.get(link, function(response) {
   response.pipe(file);
 });
 };
