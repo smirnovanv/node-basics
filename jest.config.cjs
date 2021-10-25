3c180825c88e315659344d5ecf74ab068400acb6
Natalya Smirnova <natalya.smirnova@akvelon.com>
2021-10-25 19:04:13 +0300

    Fix jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/**/__tests__/**/*.spec.ts'],
+  testMatch: ['<rootDir>/**/*.spec.ts'],
   testPathIgnorePatterns: ['/node_modules/'],
   coverageDirectory: './coverage',
   coveragePathIgnorePatterns: ['node_modules', 'src/database', 'src/test', 'src/types'],
