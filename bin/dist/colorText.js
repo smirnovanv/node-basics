be9e5a2dd7ed914a0ac15fe8d39c81b216feab98
Natalya Smirnova <natalya.smirnova@akvelon.com>
2021-11-01 17:14:02 +0300

    Installed packages

--- bin/dist/colorText.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cyanText = void 0;
-const Reset = '\x1b[0m';
-const FgCyan = '\x1b[36m';
-const cyanText = (str) => `${FgCyan}${str}${Reset}`;
-exports.cyanText = cyanText;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29sb3JUZXh0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2NvbG9yVGV4dC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxNQUFNLEtBQUssR0FBRyxTQUFTLENBQUM7QUFDeEIsTUFBTSxNQUFNLEdBQUcsVUFBVSxDQUFDO0FBQ25CLE1BQU0sUUFBUSxHQUFHLENBQUMsR0FBVyxFQUFFLEVBQUUsQ0FBQyxHQUFHLE1BQU0sR0FBRyxHQUFHLEdBQUcsS0FBSyxFQUFFLENBQUM7QUFBdEQsUUFBQSxRQUFRLFlBQThDIn0=
--- bin/dist/formatDateForTable.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatDateForTable = void 0;
-const formatDateForTable = (date) => date.toLocaleString('en-US', { hour12: false });
-exports.formatDateForTable = formatDateForTable;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZm9ybWF0RGF0ZUZvclRhYmxlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2Zvcm1hdERhdGVGb3JUYWJsZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBTyxNQUFNLGtCQUFrQixHQUFHLENBQUMsSUFBVSxFQUFFLEVBQUUsQ0FBQyxJQUFJLENBQUMsY0FBYyxDQUFDLE9BQU8sRUFBRSxFQUFFLE1BQU0sRUFBRSxLQUFLLEVBQUUsQ0FBQyxDQUFDO0FBQXJGLFFBQUEsa0JBQWtCLHNCQUFtRSJ9
